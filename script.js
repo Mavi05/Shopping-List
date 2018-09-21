82496a10d2805e25acc67ce0ee036c37b5c899b4
Mavi05 <manishmavi823@gmail.com>
2018-09-21 13:23:22 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+var button = document.getElementById("button");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+function inputLength()
+{
+	return input.value.length; 
+}
+
+function createElement()
+{
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value=""; 
+}
+
+button.addEventListener("click", function(){
+	if(inputLength() > 0)
+		createElement();
+})
+
+input.addEventListener("keypress", function(event){
+	if(inputLength() > 0 && event.keyCode === 13)
+		createElement();
+
+})
